8cdfb7a2814d5005a898a22da44623c9e0c28e25
pausegales <pau.segales01@estudiant.upf.edu>
2023-05-29 19:30:28 +0200

    update on scene

--- Assets/Scripts/Objects/pressRestart.cs
+++ Assets/Scripts/Objects/pressRestart.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement; //So you can use SceneManager
 
 public class pressRestart : MonoBehaviour
 {
+    
+    private bool p1;
+    private bool p2;
     //private bool isPressed = false;
 
     // Start is called before the first frame update
     void Start()
     {
     }
+    
+    void Update()
+    {
+        if (p1 && p2){
+            SceneManager.LoadScene("Level1"); //Load scene called Game
+        }
+    }
 
     private void OnTriggerEnter (Collider other){
-        if ( (other.CompareTag("Player1") || other.CompareTag("Player2"))){
-            SceneManager.LoadScene("Level1"); //Load scene called Game
+        if (other.CompareTag("Player1")){
+            p1 = true;
+        }
+        if (other.CompareTag("Player2")){
+           p2 = true;
         }
     }
 }
