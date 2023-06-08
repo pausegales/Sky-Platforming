d148fa2d1fc4dcc1d18b47dd35cdde716647677e
pausegales <pau.segales01@estudiant.upf.edu>
2023-06-08 23:51:18 +0200

    Night updates

--- Assets/Scripts/Platforms/movement.cs
+++ Assets/Scripts/Platforms/movement.cs
@@ -23,6 +23,7 @@ public class movement : MonoBehaviour
     public int boundaryy_dw;
     public Space space;
     public Vector3 movementDirection;
+    public bool independent_move;
 
 
     private void Update()
@@ -32,19 +33,73 @@ public class movement : MonoBehaviour
         b2_press = button2.GetComponent<pressButton>().active;
         b3_press = button3.GetComponent<pressButton>().active;
         
-        //General platform movement
-        if (b1_press || b2_press || b3_press){
+        //Independent movement from buttons
+        if (independent_move){
             transform.Translate(movementDirection * movementSpeed * Time.deltaTime, space);
             
             //Check if object reached boundaries
             if (transform.position.x >= boundaryx_up || transform.position.x <= boundaryx_dw)
             {
+//                Vector3 pos = new Vector3(0,transform.position.y,transform.position.z);
+//                if ( transform.position.x >= boundaryx_up ) {
+//                    pos.x = boundaryx_up - 2;
+//                    transform.position = pos;
+//                }
+//                if ( transform.position.x <= boundaryx_dw ) {
+//                    pos.x = boundaryx_dw + 2;
+//                    transform.position = pos;
+//                }
                 //Change movement direction
                 movementDirection.x = -movementDirection.x;
             }
             
             if (transform.position.z >= boundaryy_up || transform.position.z <= boundaryy_dw)
             {
+//                Vector3 pos = new Vector3(transform.position.x, transform.position.y,0);
+//                if ( transform.position.z >= boundaryy_up ) {
+//                    pos.z = boundaryy_up - 2;
+//                    transform.position = pos;
+//                }
+//                if ( transform.position.z <= boundaryy_dw ) {
+//                    pos.z = boundaryy_dw + 2;
+//                    transform.position = pos;
+//                }
+                //Change movement direction
+                movementDirection.z = -movementDirection.z;
+            }
+        }
+        
+        //General platform movement
+        if ((b1_press || b2_press || b3_press) && !independent_move) {
+            transform.Translate(movementDirection * movementSpeed * Time.deltaTime, space);
+            
+            //Check if object reached boundaries
+            if (transform.position.x >= boundaryx_up || transform.position.x <= boundaryx_dw)
+            {
+//                Vector3 pos = new Vector3(0,transform.position.y,transform.position.z);
+//                if ( transform.position.x >= boundaryx_up ) {
+//                    pos.x = boundaryx_up - 2;
+//                    transform.position = pos;
+//                }
+//                if ( transform.position.x <= boundaryx_dw ) {
+//                    pos.x = boundaryx_dw + 2;
+//                    transform.position = pos;
+//                }
+                //Change movement direction
+                movementDirection.x = -movementDirection.x;
+            }
+            
+            if (transform.position.z >= boundaryy_up || transform.position.z <= boundaryy_dw)
+            {
+//                Vector3 pos = new Vector3(transform.position.x, transform.position.y,0);
+//                if ( transform.position.z >= boundaryy_up ) {
+//                    pos.z = boundaryy_up - 2;
+//                    transform.position = pos;
+//                }
+//                if ( transform.position.z <= boundaryy_dw ) {
+//                    pos.z = boundaryy_dw + 2;
+//                    transform.position = pos;
+//                }
                 //Change movement direction
                 movementDirection.z = -movementDirection.z;
             }
