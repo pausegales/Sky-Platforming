ee6c249fa4371af8e9679c7810d108ff8cac39e6
pausegales <pau.segales01@estudiant.upf.edu>
2023-06-08 14:33:50 +0200

    Intro and outro titles added

--- Assets/Scripts/Managers/nextLevel.cs
+++ Assets/Scripts/Managers/nextLevel.cs
@@ -9,16 +9,15 @@ public class nextLevel : MonoBehaviour
     public GameObject final_button1;
     public GameObject final_button2;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+        
         bool b1 = final_button1.GetComponent<pressButton>().isPressed;
         bool b2 = final_button2.GetComponent<pressButton>().isPressed;
         
